9178b5097f5b6379abb7d67692712b5d6797ec27
Samagent97 <sammccarthy090@gmail.com>
2026-06-15 15:11:02 +0100

    Fix Android Compose build settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,19 @@ android {
         versionName = "1.0.0"
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
